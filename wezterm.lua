70736f1d1e3641482f867aa2de089b928bc94fd8
Lazytangent <peter.tn.mai@gmail.com>
2022-06-19 11:51:50 -0500

    Add catppuccin colors

--- wezterm.lua
+++ wezterm.lua
@@ -1,3 +1,8 @@
-return {
+local wezterm = require("wezterm")
+local catppuccin = require("colors.catppuccin").setup {}
 
+return {
+  font = wezterm.font("Fira Code"),
+  font_size = 12.5,
+  colors = catppuccin,
 }
