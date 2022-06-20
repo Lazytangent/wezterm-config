1f330ebf7b4732c1a48b5f919144257e438d0e31
Lazytangent <peter.tn.mai@gmail.com>
2022-06-20 18:32:27 -0500

    Update bindings

--- keys.lua
+++ keys.lua
@@ -1,12 +1,57 @@
 local wezterm = require("wezterm")
+local action = wezterm.action
 
-return {
-  {key="s", mods="LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-  {key="v", mods="LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-  {key="x", mods="LEADER", action=wezterm.action{CloseCurrentPane={confirm=true}}},
-  {key="]", mods="LEADER", action="ActivateCopyMode"},
-  {key="n", mods="LEADER", action=wezterm.action{ActivateTabRelative=1}},
-  {key="p", mods="LEADER", action=wezterm.action{ActivateTabRelative=-1}},
+local LEADER = "LEADER"
 
-  {key="a", mods="LEADER|CTRL", action=wezterm.action{SendString="\x01"}},
+wezterm.on("update-right-status", function(window, pane)
+  local name = window:active_key_table()
+  if name then
+    name = "TABLE: " .. name .. " "
+  end
+  window:set_right_status(name or "")
+end)
+
+local add_leader_keybind = function(keys_table, bind)
+  local keybind = bind or {}
+  keybind.mods = bind.mods or LEADER
+
+  table.insert(keys_table, keybind)
+end
+
+local keys = {}
+
+local keysbinds = {
+  {key="s", action=action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+  {key="v", action=action{SplitVertical={domain="CurrentPaneDomain"}}},
+  {key="x", action=action{CloseCurrentPane={confirm=true}}},
+  {key="[", action="ActivateCopyMode"},
+  {key="n", action=action{ActivateTabRelative=1}},
+  {key="p", action=action{ActivateTabRelative=-1}},
+  {key="c", action=action{SpawnTab="CurrentPaneDomain"}},
+  {key="t", action=action{SpawnTab="DefaultDomain"}},
+  {key="w", action=action{ShowLauncherArgs={flags="DOMAINS"}}},
+
+  {key="m", mods=LEADER, action=action{
+    ActivateKeyTable={
+      name="localleader",
+      one_shot=false,
+      replace_current=false,
+      timeout_milliseconds=1000,
+    },
+  }},
+
+  {key="a", mods="LEADER|CTRL", action=action{SendString="\x01"}},
 }
+
+for _, keybind in ipairs(keysbinds) do
+  add_leader_keybind(keys, keybind)
+end
+
+for i = 1, 8 do
+  add_leader_keybind(keys, {
+    key=tostring(i),
+    action=action{ActivateTab=i-1},
+  })
+end
+
+return keys
