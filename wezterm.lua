9a6418e05035edeaa323b2f6d33d0416349e7623
Lazytangent <peter.tn.mai@gmail.com>
2022-06-20 18:32:42 -0500

    Update settings

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,5 @@
 local wezterm = require("wezterm")
-local catppuccin = require("colors.catppuccin").setup { flavour = "mocha" }
+local catppuccin = require("colors.catppuccin").setup({flavour = "mocha"})
 
 require("tab_bar")
 local leader = require("keys.leader")
@@ -7,13 +7,14 @@ local keys = require("keys")
 local key_tables = require("keys.tables")
 
 return {
-  colors             = catppuccin,
-  font               = wezterm.font("Fira Code"),
-  font_size          = 12.5,
-  key_tables         = key_tables,
-  keys               = keys,
-  leader             = leader,
-  tab_max_width      = 50,
-  use_fancy_tab_bar  = false,
-  window_decorations = "RESIZE",
+    adjust_window_size_when_changing_font_size = false,
+    colors = catppuccin,
+    font = wezterm.font("Fira Code"),
+    font_size = 12.5,
+    key_tables = key_tables,
+    keys = keys,
+    leader = leader,
+    tab_max_width = 50,
+    use_fancy_tab_bar = false,
+    window_decorations = "RESIZE"
 }
