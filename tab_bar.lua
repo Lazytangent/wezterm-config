5c8549ce4a5e55c93e208105db4357d3c6276acd
Lazytangent <peter.tn.mai@gmail.com>
2022-06-20 11:36:49 -0500

    Start configuring tab bar

--- tab_bar.lua
+++ tab_bar.lua
@@ -1,3 +1,21 @@
-return {
+local wezterm = require("wezterm")
 
-}
+local right = utf8.char(0xe0ba)
+local left = utf8.char(0xe0bc)
+
+local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local title = wezterm.truncate_right(tab.active_pane.title, max_width-2)
+
+  return {
+    {Background={Color="#2b2042"}},
+    {Foreground={Color="#eeeeee"}},
+    {Text=left},
+    {Foreground={Color="#eeeeee"}},
+    {Text=title},
+    {Background={Color="#0b0022"}},
+    {Foreground={Color="#eeeeee"}},
+    {Text=right},
+  }
+end)
--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local catppuccin = require("colors.catppuccin").setup {}
 
-local tab_bar = require("tab_bar")
+require("tab_bar")
 local leader = require("keys.leader")
 local keys = require("keys")
 local key_tables = require("keys.tables")
@@ -10,7 +10,6 @@ return {
   font = wezterm.font("Fira Code", {bold=true}),
   font_size = 12.5,
   colors = catppuccin,
-  tab_bar_style = tab_bar_style,
   use_fancy_tab_bar = false,
   window_decorations = "RESIZE",
   leader = leader,
