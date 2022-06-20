6a05e2a2ada89d1829ac413ae6cbe3c13212a4a8
Lazytangent <peter.tn.mai@gmail.com>
2022-06-20 18:34:54 -0500

    Formatting

--- wezterm.lua
+++ wezterm.lua
@@ -7,14 +7,14 @@ local keys = require("keys")
 local key_tables = require("keys.tables")
 
 return {
-    adjust_window_size_when_changing_font_size = false,
-    colors = catppuccin,
-    font = wezterm.font("Fira Code"),
-    font_size = 12.5,
-    key_tables = key_tables,
-    keys = keys,
-    leader = leader,
-    tab_max_width = 50,
-    use_fancy_tab_bar = false,
-    window_decorations = "RESIZE"
+  adjust_window_size_when_changing_font_size = false,
+  colors = catppuccin,
+  font = wezterm.font("Fira Code"),
+  font_size = 12.5,
+  key_tables = key_tables,
+  keys = keys,
+  leader = leader,
+  tab_max_width = 50,
+  use_fancy_tab_bar = false,
+  window_decorations = "RESIZE"
 }
