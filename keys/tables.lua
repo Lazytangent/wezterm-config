e73310f5091f4d7e8ab36efe1129d47c4a5a999c
Lazytangent <peter.tn.mai@gmail.com>
2022-06-20 20:05:46 -0500

    Add more key tables

--- keys/tables.lua
+++ keys/tables.lua
@@ -1,8 +1,17 @@
 local wezterm = require("wezterm")
+local ctrl_s = require("keys.ctrl_s")
+
 local action = wezterm.action
 
 return {
   localleader = {
     {key="Escape", action="PopKeyTable"},
   },
+
+  launchers = {
+    {key="d", action="ShowLauncher"},
+    {key="Escape", action="PopKeyTable"},
+  },
+
+  ctrl_s = ctrl_s,
 }
