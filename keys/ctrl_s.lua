c22894a9e46ed7a1407eb4edaee8b1c7406c9102
Lazytangent <peter.tn.mai@gmail.com>
2022-06-21 09:22:30 -0500

    Add toggle for ligatures

--- keys/ctrl_s.lua
+++ keys/ctrl_s.lua
@@ -1,4 +1,6 @@
-local action = require("wezterm").action
+local wezterm = require("wezterm")
+local action = wezterm.action
+local action_callback = wezterm.action_callback
 
 return {
   {key="s", mods="CTRL", action=action.SendString("\x13")},
@@ -9,4 +11,13 @@ return {
   {key="l", action=action.AdjustPaneSize{"Right", 5}},
   {key=",", action=action.MoveTabRelative(-1)},
   {key=".", action=action.MoveTabRelative(1)},
+  {key="1", action=action_callback(function(win, pane)
+    local overrides = win:get_config_overrides() or {}
+    if not overrides.harfbuzz_features then
+      overrides.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
+    else
+      overrides.harfbuzz_features = nil
+    end
+    win:set_config_overrides(overrides)
+  end)},
 }
