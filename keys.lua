496bd92a4638edab00962125bdfcad785d0f09d3
Lazytangent <peter.tn.mai@gmail.com>
2022-06-20 20:05:51 -0500

    Update bindings

--- keys.lua
+++ keys.lua
@@ -1,5 +1,8 @@
 local wezterm = require("wezterm")
-local add_leader_keybind = require("utils").add_leader_keybind
+local u = require("utils")
+
+local add_keybind = u.add_keybind
+local add_leader_keybind = u.add_leader_keybind
 
 local action = wezterm.action
 
@@ -16,8 +19,8 @@ end)
 local keys = {}
 
 local keysbinds = {
-  {key="s", action=action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-  {key="v", action=action{SplitVertical={domain="CurrentPaneDomain"}}},
+  {key="s", action=action{SplitVertical={domain="CurrentPaneDomain"}}},
+  {key="v", action=action{SplitHorizontal={domain="CurrentPaneDomain"}}},
   {key="x", action=action{CloseCurrentPane={confirm=true}}},
   {key="[", action="ActivateCopyMode"},
   {key="n", action=action{ActivateTabRelative=1}},
@@ -25,11 +28,20 @@ local keysbinds = {
   {key="c", action=action{SpawnTab="CurrentPaneDomain"}},
   {key="t", action=action{SpawnTab="DefaultDomain"}},
   {key="w", action=action{ShowLauncherArgs={flags="DOMAINS"}}},
+  {key="d", action=action{DetachDomain="CurrentPaneDomain"}},
 
   {key="m", mods=LEADER, action=action{
     ActivateKeyTable={
       name="localleader",
       one_shot=false,
+      replace_current=false,
+      timeout_milliseconds=1000,
+    },
+  }},
+  {key="l", mods="LEADER|CTRL", action=action{
+    ActivateKeyTable={
+      name="launchers",
+      one_shot=true,
       replace_current=false,
       timeout_milliseconds=1000,
     },
@@ -48,5 +60,14 @@ for i = 1, 8 do
     action=action{ActivateTab=i-1},
   })
 end
+
+add_keybind(keys, {key="s", mods="CTRL", action=action{
+  ActivateKeyTable={
+    name="ctrl_s",
+    one_shot=true,
+    replace_current=false,
+    timeout_milliseconds=1000,
+  },
+}})
 
 return keys
