0b8627ea88fdbf594ff068ab91cbfee99547113e
Lazytangent <peter.tn.mai@gmail.com>
2022-06-20 20:05:39 -0500

    Add ctrl_s bindings

---
+++ keys/ctrl_s.lua
@@ -0,0 +1,12 @@
+local action = require("wezterm").action
+
+return {
+  {key="s", mods="CTRL", action=action{SendString="\x13"}},
+
+  {key="h", action=action{AdjustPaneSize={"Left", 5}}},
+  {key="j", action=action{AdjustPaneSize={"Down", 5}}},
+  {key="k", action=action{AdjustPaneSize={"Up", 5}}},
+  {key="l", action=action{AdjustPaneSize={"Right", 5}}},
+  {key=",", action=action{MoveTabRelative=-1}},
+  {key=".", action=action{MoveTabRelative=1}},
+}
