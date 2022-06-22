a785872005074b45a43bb83f3d8b2b9660fc3c70
Lazytangent <peter.tn.mai@gmail.com>
2022-06-21 21:14:20 -0500

    Use constant

--- wezterm.lua
+++ wezterm.lua
@@ -5,6 +5,7 @@ require("events")
 local leader = require("keys.leader")
 local keys = require("keys")
 local key_tables = require("keys.tables")
+local utils = require("utils")
 
 return {
   adjust_window_size_when_changing_font_size = false,
@@ -14,7 +15,7 @@ return {
   key_tables = key_tables,
   keys = keys,
   leader = leader,
-  tab_max_width = 50,
+  tab_max_width = utils.MAX_TAB_LENGTH,
   use_fancy_tab_bar = false,
   window_decorations = "RESIZE"
 }
