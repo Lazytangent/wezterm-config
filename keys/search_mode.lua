eb1438c4b596999dc4d4846828f5def2a3119ba2
Lazytangent <peter.tn.mai@gmail.com>
2022-06-21 21:26:41 -0500

    Sort and add Ctrl-c

--- keys/search_mode.lua
+++ keys/search_mode.lua
@@ -2,16 +2,18 @@ local wezterm = require("wezterm")
 local action = wezterm.action
 
 return {
-  {key="Escape", mods="NONE", action=action.CopyMode("Close")},
   {key="Enter",  mods="NONE", action=action.ActivateCopyMode},
+  {key="Escape", mods="NONE", action=action.CopyMode("Close")},
 
   -- Defaults
-  {key="UpArrow",   mods="NONE", action=action.CopyMode("PriorMatch")},
-  {key="p",         mods="CTRL", action=action.CopyMode("PriorMatch")},
-  {key="PageUp",    mods="NONE", action=action.CopyMode("PriorMatchPage")},
-  {key="PageDown",  mods="NONE", action=action.CopyMode("NextMatchPage")},
-  {key="n",         mods="CTRL", action=action.CopyMode("NextMatchPage")},
   {key="DownArrow", mods="NONE", action=action.CopyMode("NextMatch")},
-  {key="r",         mods="CTRL", action=action.CopyMode("CycleMatchType")},
-  {key="u",         mods="CTRL", action=action.CopyMode("ClearPattern")}
+  {key="PageDown",  mods="NONE", action=action.CopyMode("NextMatchPage")},
+  {key="PageUp",    mods="NONE", action=action.CopyMode("PriorMatchPage")},
+  {key="UpArrow",   mods="NONE", action=action.CopyMode("PriorMatch")},
+
+  {key="c", mods="CTRL", action=action.CopyMode("Close")},
+  {key="n", mods="CTRL", action=action.CopyMode("NextMatchPage")},
+  {key="p", mods="CTRL", action=action.CopyMode("PriorMatch")},
+  {key="r", mods="CTRL", action=action.CopyMode("CycleMatchType")},
+  {key="u", mods="CTRL", action=action.CopyMode("ClearPattern")},
 }
