4eef355acd1ce1a929dc27b197e125e256ebe9ba
Lazytangent <peter.tn.mai@gmail.com>
2022-07-02 12:12:51 -0500

    Fix for Copy Mode with truncated title

--- events/tab_bar.lua
+++ events/tab_bar.lua
@@ -15,7 +15,7 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
   local original_title = pane.title
 
   local title_text = include_copy_prefix(replace_home(remove_file(pane.current_working_dir)), original_title)
-  title_text = shorten(title_text)
+  title_text = include_copy_prefix(shorten(title_text), original_title)
   local title = pad_with_space(tab.tab_index .. ": " .. title_text)
 
   local right_edge_background = "#1e1e2e"
